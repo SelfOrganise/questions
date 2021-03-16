7791299a5adedc641b9f642195ab5b9be136bcc2
Bogdan Boiculese <bogdan.boiculese@doctorlink.com>
2021-03-16 15:05:54 +0000

    More env

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,12 @@
-import { ApolloServer, ServerInfo } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import * as express from 'express';
 import { schema } from './schemas';
 
+const app = express();
 const server = new ApolloServer({ schema, tracing: process.env.NODE_ENV !== 'production' });
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }: ServerInfo) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.applyMiddleware({ app });
+
+const port = process.env.PORT || 4000;
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+);
